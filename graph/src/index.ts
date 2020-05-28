8eb90ba4a04f000b4863222aeff6243f2e21019f
Signe Grau Kristensen <mkris16@student.sdu.dk>
2020-05-28 18:52:24 +0200

    Group in bands

--- graph/src/index.ts
+++ graph/src/index.ts
@@ -6,6 +6,10 @@ interface Node {
     id: string;
     name: string;
     group: string;
+    x?: number;
+    y?: number;
+    size?: number;
+    group_data?: string;
 }
 
 interface Edge {
@@ -30,7 +34,6 @@ const getData = async (): Promise<Data> => {
     return await new Promise((resolve) => parse(text, {
         delimiter: ';',
         relax_column_count: true,
-        to: 100,
     }).on('readable', function () {
         let record: ReadonlyArray<string>;
         while (record = this.read()) {
@@ -104,6 +107,119 @@ const getData = async (): Promise<Data> => {
     }));
 }
 
+const network = (data: Data, prev: Data | undefined, index: (n: string) => string, expand) => {
+    expand = expand || {};
+    let gm = {},    // group map
+        nm = {},    // node map
+        lm = {},    // link map
+        gn = {},    // previous group nodes
+        gc = {},    // previous group centroids
+        nodes = [], // output nodes
+        links = []; // output links
+
+    // process previous nodes for reuse or centroid calculation
+    if (prev) {
+        prev.nodes.forEach(function(n) {
+            let i = index(n.id), o;
+            if (n.size > 0) {
+                gn[i] = n;
+                n.size = 0;
+            } else {
+                o = gc[i] || (gc[i] = {x:0,y:0,count:0});
+                o.x += n.x;
+                o.y += n.y;
+                o.count += 1;
+            }
+        });
+    }
+
+    // determine nodes
+    for (let k=0; k<data.nodes.length; ++k) {
+        let n = data.nodes[k],
+            i = index(n.id),
+            l = gm[i] || (gm[i]=gn[i]) || (gm[i]={id:i, name:i, group:i, size:0, nodes:[]});
+
+        if (expand[i]) {
+            // the node should be directly visible
+            nm[n.id] = nodes.length;
+            nodes.push(n);
+            if (gn[i]) {
+                // place new nodes at cluster location (plus jitter)
+                n.x = gn[i].x + Math.random();
+                n.y = gn[i].y + Math.random();
+            }
+        } else {
+            // the node is part of a collapsed cluster
+            if (l.size == 0) {
+                // if new cluster, add to set and position at centroid of leaf nodes
+                nm[i] = nodes.length;
+                nodes.push(l);
+                if (gc[i]) {
+                    l.x = gc[i].x / gc[i].count;
+                    l.y = gc[i].y / gc[i].count;
+                }
+            }
+            l.nodes.push(n);
+        }
+        // always count group size as we also use it to tweak the force graph strengths/distances
+        l.size += 1;
+        n.group_data = l;
+    }
+
+    for (const i in gm) { gm[i].link_count = 0; }
+
+    // determine links
+    for (let k=0; k<data.edges.length; ++k) {
+        let e = data.edges[k],
+            u = index(e.source),
+            v = index(e.target);
+        if (u != v) {
+            gm[u].link_count++;
+            gm[v].link_count++;
+        }
+        u = expand[u] ? nm[e.source] : u;
+        v = expand[v] ? nm[e.target] : v;
+        let i = (u<v ? u+"|"+v : v+"|"+u),
+            l = lm[i] || (lm[i] = {source:u, target:v, size:0});
+        l.size += 1;
+    }
+    for (const i in lm) { links.push(lm[i]); }
+
+    return {nodes: nodes, links: links};
+}
+
+const convexHulls = (nodes, index, offset) => {
+    let hulls = {};
+
+    // create point sets
+    for (let k=0; k<nodes.length; ++k) {
+        const n = nodes[k];
+        if (n.size) continue;
+        const i = index(n),
+            l = hulls[i] || (hulls[i] = []);
+        l.push([n.x-offset, n.y-offset]);
+        l.push([n.x-offset, n.y+offset]);
+        l.push([n.x+offset, n.y-offset]);
+        l.push([n.x+offset, n.y+offset]);
+    }
+
+    // create convex hulls
+    const hullset = [];
+    for (let i in hulls) {
+        hullset.push({group: i, path: d3.geom.hull(hulls[i])});
+    }
+
+    return hullset;
+}
+
+const curve = d3.line()
+    .curve(d3.curveCardinalClosed);
+
+const drawCluster = (d) => {
+    return curve(d.path); // 0.8
+}
+
+
 const drag = simulation => {
 
     function dragstarted(d) {
@@ -136,33 +252,37 @@ const makeChart = (data: Data) => {
     const height = 4800;
     const width = 6400;
 
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force('collision', d3.forceCollide().radius(0.5));
-
 
     const svg = d3.create("svg")
-        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+        .attr("viewBox", [0, 0, width, height]);
 
     const root = svg.append("g")
         .attr("class", "everything");
+
+    const net = network(data, null, (n: string) => n.split('|')[1], {});
+    console.log(net);
+
+    const simulation = d3.forceSimulation(net.nodes)
+        .force("link", d3.forceLink(net.links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force('collision', d3.forceCollide().radius(d => d.nodes.length))
+        .force('center', d3.forceCenter(width / 2, height / 2));
 
     const link = root.append("g")
         .attr("stroke", "#f00")
         .attr("stroke-opacity", 0.8)
         .selectAll("line")
-        .data(links)
+        .data(net.links)
         .join("line")
-        .attr("stroke-width", d => Math.sqrt(Math.pow(d.value, 2)));
+        .attr("stroke-width", d => Math.sqrt(Math.pow(d.size, 2)));
 
     const node = root.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.0)
         .selectAll("circle")
-        .data(nodes)
+        .data(net.nodes)
         .join("circle")
-        .attr("r", 5)
+        .attr("r", d => d.nodes.length)
         .attr("fill", "black")
         .call(drag(simulation));
 
