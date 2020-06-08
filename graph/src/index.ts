ac4e25f02de45b574c0bdef968d5fb02e278f4ad
Signe Grau Kristensen <mkris16@student.sdu.dk>
2020-06-08 19:51:09 +0200

    Expanding nodes

--- graph/src/index.ts
+++ graph/src/index.ts
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import * as d3Zoom from "d3-zoom";
+import * as d3Polygon from "d3-polygon";
 import parse = require("csv-parse");
 
 interface Node {
@@ -34,6 +35,7 @@ const getData = async (): Promise<Data> => {
     return await new Promise((resolve) => parse(text, {
         delimiter: ';',
         relax_column_count: true,
+        to_line: 100
     }).on('readable', function () {
         let record: ReadonlyArray<string>;
         while (record = this.read()) {
@@ -107,8 +109,7 @@ const getData = async (): Promise<Data> => {
     }));
 }
 
-const network = (data: Data, prev: Data | undefined, index: (n: string) => string, expand) => {
-    expand = expand || {};
+const network = (prev, index: (n: string) => string, expand: string | undefined) => {
     let gm = {},    // group map
         nm = {},    // node map
         lm = {},    // link map
@@ -119,13 +120,13 @@ const network = (data: Data, prev: Data | undefined, index: (n: string) => strin
 
     // process previous nodes for reuse or centroid calculation
     if (prev) {
-        prev.nodes.forEach(function(n) {
+        prev.nodes.forEach(function (n) {
             let i = index(n.id), o;
             if (n.size > 0) {
                 gn[i] = n;
                 n.size = 0;
             } else {
-                o = gc[i] || (gc[i] = {x:0,y:0,count:0});
+                o = gc[i] || (gc[i] = {x: 0, y: 0, count: 0});
                 o.x += n.x;
                 o.y += n.y;
                 o.count += 1;
@@ -134,14 +135,20 @@ const network = (data: Data, prev: Data | undefined, index: (n: string) => strin
     }
 
     // determine nodes
-    for (let k=0; k<data.nodes.length; ++k) {
-        let n = data.nodes[k],
-            i = index(n.id),
-            l = gm[i] || (gm[i]=gn[i]) || (gm[i]={id:i, name:i, group:i, size:0, nodes:[]});
+    for (let k = 0; k < data.nodes.length; ++k) {
+        let n = data.nodes[k];
+        let i = index(n.id);
 
-        if (expand[i]) {
+        if (gm[i] == undefined) {
+            console.log(gn[i]);
+            gm[i] = gn[i] || {id: i, name: i, group: i, size: 0, nodes: []};
+        }
+
+        let l = gm[i];
+
+        if (expand === i) {
             // the node should be directly visible
-            nm[n.id] = nodes.length;
+            nm[n.id] = n.id;
             nodes.push(n);
             if (gn[i]) {
                 // place new nodes at cluster location (plus jitter)
@@ -151,6 +158,7 @@ const network = (data: Data, prev: Data | undefined, index: (n: string) => strin
         } else {
             // the node is part of a collapsed cluster
             if (l.size == 0) {
+                l.nodes = [];
                 // if new cluster, add to set and position at centroid of leaf nodes
                 nm[i] = nodes.length;
                 nodes.push(l);
@@ -166,10 +174,12 @@ const network = (data: Data, prev: Data | undefined, index: (n: string) => strin
         n.group_data = l;
     }
 
-    for (const i in gm) { gm[i].link_count = 0; }
+    for (const i in gm) {
+        gm[i].link_count = 0;
+    }
 
     // determine links
-    for (let k=0; k<data.edges.length; ++k) {
+    for (let k = 0; k < data.edges.length; ++k) {
         let e = data.edges[k],
             u = index(e.source),
             v = index(e.target);
@@ -177,13 +187,16 @@ const network = (data: Data, prev: Data | undefined, index: (n: string) => strin
             gm[u].link_count++;
             gm[v].link_count++;
         }
-        u = expand[u] ? nm[e.source] : u;
-        v = expand[v] ? nm[e.target] : v;
-        let i = (u<v ? u+"|"+v : v+"|"+u),
-            l = lm[i] || (lm[i] = {source:u, target:v, size:0});
+        let internal = u == v;
+        u = expand == u ? nm[e.source] : u;
+        v = expand == v ? nm[e.target] : v;
+        let i = (u < v ? u + "|" + v : v + "|" + u),
+            l = lm[i] || (lm[i] = {source: u, target: v, size: 0, internal: internal});
         l.size += 1;
     }
-    for (const i in lm) { links.push(lm[i]); }
+    for (const i in lm) {
+        links.push(lm[i]);
+    }
 
     return {nodes: nodes, links: links};
 }
@@ -192,21 +205,21 @@ const convexHulls = (nodes, index, offset) => {
     let hulls = {};
 
     // create point sets
-    for (let k=0; k<nodes.length; ++k) {
+    for (let k = 0; k < nodes.length; ++k) {
         const n = nodes[k];
         if (n.size) continue;
-        const i = index(n),
+        const i = index(n.id),
             l = hulls[i] || (hulls[i] = []);
-        l.push([n.x-offset, n.y-offset]);
-        l.push([n.x-offset, n.y+offset]);
-        l.push([n.x+offset, n.y-offset]);
-        l.push([n.x+offset, n.y+offset]);
+        l.push([n.x - offset, n.y - offset]);
+        l.push([n.x - offset, n.y + offset]);
+        l.push([n.x + offset, n.y - offset]);
+        l.push([n.x + offset, n.y + offset]);
     }
 
     // create convex hulls
     const hullset = [];
     for (let i in hulls) {
-        hullset.push({group: i, path: d3.geom.hull(hulls[i])});
+        hullset.push({group: i, path: d3Polygon.polygonHull(hulls[i])});
     }
 
     return hullset;
@@ -245,57 +258,92 @@ const drag = simulation => {
         .on("end", dragended);
 }
 
-const makeChart = (data: Data) => {
-    const links: ReadonlyArray<Edge> = data.edges.map(d => Object.create(d));
-    const nodes: ReadonlyArray<Node> = data.nodes.map(d => Object.create(d));
+let data, svg, root, net, simulation, link, node, hull, zoomHandler;
+let height = 4800;
+let width = 6400;
 
-    const height = 4800;
-    const width = 6400;
+const init = (initData: Data) => {
+    data = initData;
 
-
-    const svg = d3.create("svg")
+    svg = d3.create("svg")
         .attr("viewBox", [0, 0, width, height]);
 
-    const root = svg.append("g")
+    root = svg.append("g")
         .attr("class", "everything");
 
-    const net = network(data, null, (n: string) => n.split('|')[1], {});
-    console.log(net);
-
-    const simulation = d3.forceSimulation(net.nodes)
-        .force("link", d3.forceLink(net.links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force('collision', d3.forceCollide().radius(d => d.nodes.length))
-        .force('center', d3.forceCenter(width / 2, height / 2));
-
-    const link = root.append("g")
-        .attr("stroke", "#f00")
-        .attr("stroke-opacity", 0.8)
-        .selectAll("line")
-        .data(net.links)
-        .join("line")
-        .attr("stroke-width", d => Math.sqrt(Math.pow(d.size, 2)));
-
-    const node = root.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.0)
-        .selectAll("circle")
-        .data(net.nodes)
-        .join("circle")
-        .attr("r", d => d.nodes.length)
-        .attr("fill", "black")
-        .call(drag(simulation));
-
-    const zoomHandler = d3Zoom.zoom().on("zoom", () => {
+    zoomHandler = d3Zoom.zoom().on("zoom", () => {
         root.attr("transform", d3.event.transform);
     });
 
     zoomHandler(svg);
 
+    makeChart(undefined);
+}
+
+const getIndex = (n: string) => n.split('|')[1] || n;
+
+const makeChart = (expand: string | undefined) => {
+    if (simulation) simulation.stop();
+
+    const height = 4800;
+    const width = 6400;
+
+    console.log(net);
+    net = network(net, getIndex, expand);
+    console.log(net);
+
+    simulation = d3.forceSimulation(net.nodes)
+        .force("link", d3.forceLink(net.links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force('collision', d3.forceCollide().radius(d => d.nodes ? d.nodes.length : 1))
+        .force('center', d3.forceCenter(width / 2, height / 2));
+
+    if (hull) hull.remove();
+    hull = root.append("g")
+        .data(convexHulls(net.nodes, getIndex, 4))
+        .append("path")
+        .attr("class", "hull")
+        .attr("d", drawCluster)
+        .style("fill", d => "#ff0")
+        .style("opacity", d => 0.4)
+        .on("click", function (d) {
+            console.log("hull click", d, arguments, this, expand[d.group]);
+            makeChart(undefined);
+        });
+
+    if (link) link.remove();
+    link = root.append("g")
+        .attr("stroke", "#f00")
+        .selectAll("line")
+        .data(net.links)
+        .join("line")
+        .attr("stroke-opacity", d => d.internal ? 0 : 0.8)
+        .attr("stroke-width", d => d.internal ? 0 : Math.sqrt(Math.pow(d.size, 2)));
+
+    if (node) node.remove();
+    node = root.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0.0)
+        .selectAll("circle")
+        .data(net.nodes)
+        .join("circle")
+        .attr("r", d => d.nodes ? d.nodes.length : 2)
+        .attr("fill", "black")
+        .call(drag(simulation))
+        .on("click", function (d) {
+            console.log("node click", d, arguments, this, expand);
+            makeChart(d.group);
+        });
+
     node.append("title")
         .text(d => d.id);
 
     simulation.on("tick", () => {
+        if (!hull.empty()) {
+            hull.data(convexHulls(net.nodes, getIndex, 4))
+                .attr("d", drawCluster);
+        }
+
         link
             .attr("x1", d => d.source.x)
             .attr("y1", d => d.source.y)
@@ -306,15 +354,11 @@ const makeChart = (data: Data) => {
             .attr("cx", d => d.x)
             .attr("cy", d => d.y);
     });
-
-//invalidation.then(() => simulation.stop());
-
-    return svg.node();
 }
 
 getData().then((data) => {
     console.log(data.nodes.length);
     console.log(data.edges.length);
-    const svg = makeChart(data);
-    document.body.append(svg);
+    init(data);
+    document.body.append(svg.node());
 });
